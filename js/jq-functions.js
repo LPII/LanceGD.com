2215ac1a547e3188f562e9441d559c770a06892b
LPII <LancePorterGD@gmail.com>
2016-06-30 15:22:30 -0400

    Fix gallery grid, Add ui styles, update favicon, add footer stuff, responsive styles, get gallery index working properly

---
+++ js/jq-functions.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+  
+  $('.xbtn').click(function() {
+      $('.featProjWrap').fadeOut();
+      $('.project-con').removeClass('vis');
+      $('body').removeClass("modalOpen");
+      return false;
+
+  });
+
+//   $('#projList li').click(function() {
+//       $('.featProjWrap').fadeIn();
+//       $('body').addClass("modalOpen");
+//       return false;
+//   });
+
+   $('#projList li').click(function() {
+
+        var index = $('#projList li').index(this) + 1;
+        $('.featProjWrap').fadeIn();
+        $('.featProjWrap').children('.project-con:nth-of-type(' + index  + ')').addClass("vis");
+      $('body').addClass("modalOpen");
+      return false;
+  });
+
+
+});
+
+
+
