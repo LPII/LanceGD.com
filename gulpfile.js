183a8a66c166377c8b32fe54d3d3a77260c7bd56
Lporter <Lanceportergd@gmail.com>
2017-12-25 16:56:35 -0500

    Updates to gulpfile organize js update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,162 @@
+//PROJECT: VIP
+
 // -- Var -- //
 
+//PLUGINS//
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
 var wait = require("gulp-wait");
-var dirSync = require('gulp-directory-sync');
+var dirSync = require("gulp-directory-sync");
 var imagemin = require('gulp-imagemin');
+var concat = require('gulp-concat');
+var clean = require('gulp-rimraf');
+var ftp = require('vinyl-ftp');
+var runSequence = require('run-sequence');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
+var localFilesGlob = ['./build/**'];
 
-// -- Tasks -- //
 
-//Compile SCSS
-gulp.task('styles', function () {
-    gulp.src('./src/scss/*.scss')
+//PROJECTS//
+var localDir = "http://localhost/Personal";
+var srcDir = "./src/";
+var buildDir = "./build/";
+var altDir = "C:/xampp/htdocs/Personal/wp-content/themes/PersonalSite/"
+
+
+
+// -- UTIL -- //
+
+//Kill build dir
+gulp.task('clean', function () {
+
+    return gulp.src('./build', {
+        read: false
+    }) // much faster
+        .pipe(wait(1000))
+        .pipe(clean());
+
+});
+
+//Spin up local server
+gulp.task('sync', function () {
+    browserSync.init({
+        proxy: localDir
+    });
+
+});
+
+//Reload Browser
+gulp.task('reload', function () {
+    return gulp.src('')
+        .pipe(wait(2000))
+        .pipe(browserSync.stream());
+});
+
+
+
+// -- BASE BUILD TASKS -- //
+
+
+//Compile SCSS,make-styles --> build
+gulp.task('make-styles', function () {
+    gulp.src(srcDir + 'scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS({
             compatibility: 'ie8'
         }))
         .pipe(rename('style.css'))
-        .pipe(gulp.dest('./src/css/'))
-    //  .pipe(wait(2000))
-    //      .pipe(browserSync.stream());
+        .pipe(gulp.dest( buildDir +'css/'))
+        .pipe(gulp.dest( altDir +'css/'))
+});
+
+//Compile JS, make-js --> build
+gulp.task('make-js', function () {
+
+    return gulp.src([srcDir + 'js/lib/*.js', srcDir + 'js/core/*.js'])
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(buildDir + 'js/'))
+        .pipe(gulp.dest(altDir + 'js/'));
 
 });
-//Make build 
-gulp.task('build', function () {
+
+//Image min, make-image --> build
+gulp.task('make-image', function () {
+    gulp.src(srcDir + 'img/**/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest(buildDir + 'img/'))
+        .pipe(gulp.dest(altDir + 'img/'))
+
+});
+
+//Build everything make-rest --> build
+gulp.task('make-rest', function () {
     return gulp.src('')
-        .pipe(dirSync('./src', './build', {
-            ignore: ['img', 'scss']
+        .pipe(dirSync(srcDir, buildDir, {
+            ignore: ['img', 'scss','css', 'js','js/**']
+        }))
+        .pipe(dirSync(srcDir, altDir, {
+            ignore: ['img', 'scss','css', 'js','js/**']
         }));
 });
-//Image min
-gulp.task('image', function () {
-    gulp.src('./src/img/**/**')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./build/img/'))
 
+//(styles, js, image, sync rest)
+gulp.task('construct', function () {
+    runSequence('clean',
+        'make-styles',
+        'make-js',
+        'make-rest');
 });
-//Spin up local server
-gulp.task('sync', function () {
-    browserSync.init({
-        proxy: "http://localhost/DEV/LGD_dev"
+
+
+
+//-- BUILD EXTENSION --//
+
+
+//FTP
+gulp.task('ftp', function () {
+    //Connection info obejct
+    var conn = ftp.create({
+        host: 'lancegd.com',
+        user: 'admin@lancegd.com',
+        password: '%Cr6d>P2',
+        parallel: 10,
+        log: ''
     });
+    var remoteFolder = '/MMdev/wp-content/themes/MMK';
+
+    //Keep it simple and just upload build folder, worry about keeping build clean with other task
+    return gulp.src(localFilesGlob, {base: 'build', buffer: false})
+        .pipe(wait(500))
+        .pipe(conn.newer(remoteFolder))
+        .pipe(conn.dest(remoteFolder));
 });
-//Reload Browser
-gulp.task('reload', function () {
+
+//Send to dev destingation
+gulp.task('to-dev', function () {
     return gulp.src('')
-        .pipe(dirSync('./src', './build', {
-            ignore: function (dir, file) {
-                return file === 'scss';
-            }
-        }))
-        .pipe(wait(1100))
-        .pipe(browserSync.stream());
-
+        .pipe(dirSync(buildDir, 'C:/xampp/htdocs/DEV/VIP/wp-content/themes/VIP-homes', {}));
 });
-//Watcher task
+gulp.task('to-dev-reload', function () {
+    runSequence('to-dev',
+        'reload');
+});
+
+//-- WATCHERS --//
 gulp.task('watch', function () {
-    gulp.watch(['./src/scss/*.scss', 'scss/*/*.scss'], ['styles']);
-    gulp.watch(['./src/css/*.css'], ['reload']);
-    gulp.watch(['./src/img/**'], ['image']);
-    gulp.watch(['./src/js/*.js'], ['reload']);
-    gulp.watch(['./src/*.php'], ['reload']);
-
+    //Source
+    gulp.watch(['./src/*.php'], ['make-rest','reload']);
+    gulp.watch(['./src/scss/*.scss', './src/scss/*/*.scss'], ['make-styles','reload']);
+    gulp.watch(['./src/js/**/*.js'], ['make-js','reload']);
+    gulp.watch(['./src/img/**'], ['make-rest','reload']);
 });
+
 
 // -- Default -- //
-gulp.task('default', ['styles', 'build', 'image', 'sync', 'watch']);
+gulp.task('local', ['clean','construct', 'sync', 'watch']);
+gulp.task('remote', ['build', 'watch-remote']);
+gulp.task('serve', ['sync', 'watch']);
--- src/js/jq-functions.js
+++ src/js/jq-functions.js
@@ -1,47 +1,48 @@
 //Functions
 //Close project
 function closeProj() {
-  $('.featProjWrap').fadeOut();
-  $('.project-con').removeClass('vis');
-  $('body').removeClass("modalOpen");
-  return false;
+    $('.featProjWrap').fadeOut();
+    $('.project-con').removeClass('vis');
+    $('body').removeClass("modalOpen");
+    return false;
 }
+
 //Use github api to get latest tag dynamically
 function latestTag() {
-  var gitHubPath = 'LPII/LanceGD.com';
-  var url = 'https://api.github.com/repos/' + gitHubPath + '/tags';
+    var gitHubPath = 'LPII/LanceGD.com';
+    += = +
+    var url = 'https://api.github.com/repos/' + gitHubPath + '/tags';
 
-  $.get(url).done(function (data) {
-    var versions = data.sort(function (v1, v2) {
-      return semver.compare(v2.name, v1.name)
+    $.get(url).done(function (data) {
+        var versions = data.sort(function (v1, v2) {
+            return semver.compare(v2.name, v1.name)
+        });
+        $('.tag-result').html(versions[0].name);
     });
-    $('.tag-result').html(versions[0].name);
-  });
 }
-
 
 
 $(document).ready(function () {
-  //close project with close btns
-  $('.xbtn, .closproj').click(function () {
-    closeProj();
-  });
-  //click anyting to close project 
-  $(document).on('click', function (e) {
-    closeProj();
-  });
-  //clicking proj itself wont close proj
-  $('.featProjWrap .project-con .container').on('click', function (e) {
-    e.stopPropagation();
-  });
-  //open correct project
-  $('#projList li').click(function () {
-    var index = $('#projList li').index(this) + 1;
-    $('.featProjWrap').fadeIn();
-    $('.featProjWrap').children('.project-con:nth-of-type(' + index + ')').addClass("vis");
-    $('body').addClass("modalOpen");
-    return false;
-  });
-  //Latest tag
-  latestTag();
+    //close project with close btns
+    $('.xbtn, .closproj').click(function () {
+        closeProj();
+    });
+    //click anyting to close project
+    $(document).on('click', function (e) {
+        closeProj();
+    });
+    //clicking proj itself wont close proj
+    $('.featProjWrap .project-con .container').on('click', function (e) {
+        e.stopPropagation();
+    });
+    //open correct project
+    $('#projList li').click(function () {
+        var index = $('#projList li').index(this) + 1;
+        $('.featProjWrap').fadeIn();
+        $('.featProjWrap').children('.project-con:nth-of-type(' + index + ')').addClass("vis");
+        $('body').addClass("modalOpen");
+        return false;
+    });
+    //Latest tag
+    latestTag();
 });
